d9e129d6fffbb0c7f47beac2fcbb03ea58adcdcb
saka_mawa <sakadana2003@gmail.com>
2022-06-24 14:53:39 +0700

    Update

--- modules/auth/views/login.php
+++ modules/auth/views/login.php
@@ -24,8 +24,8 @@
         <div class="col-11 col-sm-11 col-md-6 col-lg-5 col-xl-3 mx-auto align-self-center py-4">
             <form id="form_login" method="POST" url="<?= base_url('func_auth/login_proses'); ?>">
                 <div class="text-center my-5">
-                    <p class="mb-1 title-1">Log in App Siswa</p>
-                    <p class="mb-0 fw-600 size-18 title-2">Masuk dengan kode sekolah, username dan kata sandi</p>
+                    <p class="mb-1 title-1">Log in App Staf</p>
+                    <p class="mb-0 fw-600 size-18 title-2">Masuk dengan Kode sekolah, User ID dan Kata sandi</p>
                 </div>
 
                 <div class="mb-4" id="req_kode_sekolah">
