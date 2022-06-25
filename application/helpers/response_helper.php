381e72ea26cd32055a59567c9d5f78f19288b24e
saka_mawa <sakadana2003@gmail.com>
2022-06-25 15:57:43 +0700

    Update

--- application/helpers/response_helper.php
+++ application/helpers/response_helper.php
@@ -6,7 +6,7 @@ function alert_show($msg, $type)
   $str = "";
   if ($msg != "") {
 
-    $str = '<div class="alert alert-' . $type . ' alert-dismissible fade show" role="alert" style="        position:fixed; top: 0px; left: 0px; width: 100%; z-index:9999;border-radius:0px;">
+    $str = '<div class="alert alert-' . $type . ' alert-dismissible fade show" role="alert" style="position:fixed; top: 0px; left: 0px; width: 100%; z-index:9999;border-radius:0px;">
     ' . $msg . '
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
