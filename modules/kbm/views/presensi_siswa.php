958e111b67de12bb53df8e7be4f4e6673d735b0a
saka_mawa <sakadana2003@gmail.com>
2022-08-15 13:01:36 +0700

    Push

--- modules/kbm/views/presensi_siswa.php
+++ modules/kbm/views/presensi_siswa.php
@@ -28,7 +28,7 @@
                             </h2>
                             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                 <div class="accordion-body size-11">
-                                    <div class="row mb-3 ps-5">
+                                    <div class="row mb-3 ps-4">
                                         <div class="col-6">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
@@ -47,7 +47,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="row ps-5">
+                                    <div class="row mb-3 ps-4">
                                         <div class="col-6">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3">
@@ -63,6 +63,12 @@
                                                     Alpha
                                                 </label>
                                             </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <button data-bs-toggle="modal" data-bs-target="#modalLihatSuratIzin" class="btn btn-block btn-md btn-danger btn-surat-izin">Lihat surat izin</button>
                                         </div>
                                     </div>
                                 </div>
@@ -90,7 +96,7 @@
                             </h2>
                             <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample2">
                                 <div class="accordion-body size-11">
-                                    <div class="row mb-3 ps-5">
+                                    <div class="row mb-3 ps-4">
                                         <div class="col-6">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault5">
@@ -109,7 +115,72 @@
                                         </div>
                                     </div>
 
-                                    <div class="row ps-5">
+                                    <div class="row ps-4">
+                                        <div class="col-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault7">
+                                                <label class="form-check-label size-15 fw-normal" for="flexRadioDefault7">
+                                                    Ijin
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault8">
+                                                <label class="form-check-label size-15 fw-normal" for="flexRadioDefault8">
+                                                    Alpha
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion accordion-flush p-0 shadow-sm mb-3" id="accordionFlushExample3">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button class="accordion-button collapsed text-wrap bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree" style="border-radius: 10px;">
+                                    <div class="badge-hadir">
+                                        <p class="text-white size-12">Hadir</p>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-auto">
+                                            <div class="avatar avatar-50 rounded-circle avatar-pesan">
+                                                <img src="<?= base_url() ?>assets/img/user4.jpg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col align-self-center ps-0">
+                                            <p class="mb-0 size-15 fw-medium">Siswa CC</p>
+                                            <p class="text-muted text-secondary size-12">Status Kehadiran</p>
+                                        </div>
+                                    </div>
+                                </button>
+
+                            </h2>
+                            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample3">
+                                <div class="accordion-body size-11">
+                                    <div class="row mb-3 ps-4">
+                                        <div class="col-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault5">
+                                                <label class="form-check-label size-15 fw-normal" for="flexRadioDefault5">
+                                                    Hadir
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault6">
+                                                <label class="form-check-label size-15 fw-normal" for="flexRadioDefault6">
+                                                    Sakit
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row ps-4">
                                         <div class="col-6">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault7">
@@ -136,5 +207,36 @@
         </div>
     </div>
 </div>
-<!-- main page content ends -->
+
 </main>
+
+
+
+<div class="modal fade show" id="modalLihatSuratIzin" tabindex="-1" aria-labelledby="exampleModalLabel" aria-modal="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header py-4">
+                <h5 class="modal-title">Detail Surat Izin</h5>
+                <button type="button" class="btn-close me-0" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body mb-0 pb-0">
+                <div class="row">
+                    <!-- jika surat izin berupa gambar -->
+                    <div class="col-12">
+                        <figure  class="overflow-hidden rounded-15 text-center detail-pengumuman" style="background-position: center; background-size: cover; background-image: url('<?= base_url('assets/img/categories1.png')?>');">
+                        </figure>
+                    </div>
+                    <!-- Jika surat izin berupa file -->
+                    <div class="col-12">
+                        <figure class="overflow-hidden rounded-15 text-center" style="background-color: #FFE6E6; padding: 40px;">
+                            <i class="fa-solid fa-file-pdf" style="font-size: 7rem;"></i>
+                        </figure>
+                        <a class="btn btn-block btn-md btn-filter" href="#">DOWNLOAD</a>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer d-flex justify-content-center border-0 mt-0 pt-0">
+            </div>
+        </div>
+    </div>
+</div>
